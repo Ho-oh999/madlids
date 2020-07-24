06fab8c1ef0c201a34df6c78de7c8d18ca586399
boko999 <ho-oh999@hotmail.com>
2020-07-24 18:18:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+name = input ("hello, what is your name?")
+print (f"hi {name}. lets start." )
+
+
+print()
+
+holiday = input ("enter a name of a holiday:" )
+place = input("enter a place:")
+hero = input("give me a hero:")
+outfit= input("wat do you want to where:")
+bad_guy= input("enter a bad guy:")
+power = input("what power do you want:")
+name = input("enter a name:")
+adjective = input("give my a adjective:")
+bad_guy2= input("give me a 2 bad guy:")
+
+print (f"last {holiday}, i went to {place} and {hero} was walking with out {outfit} after it was found {bad_guy} and {hero} beat him with {power} then {name} the mayour of {place} and said that he was very {adjective} that {hero} helped then {bad_guy2} started to robe a bank and {hero} and he ran of to stop him")
